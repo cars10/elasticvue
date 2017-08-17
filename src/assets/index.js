a6ea513722aae8840269ff58b00cf81e0b6d1f25
Carsten König <carstenkoenig92@gmail.com>
2017-08-17 10:59:17 +0200

    - adds select for indeces

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,21 +3,31 @@ import VueMaterial from 'vue-material'
 
 // Vue-Material
 import 'vue-material/dist/components/mdCore/index.css'
+import 'vue-material/dist/components/mdBackdrop/index.css'
 import 'vue-material/dist/components/mdButton/index.css'
+import 'vue-material/dist/components/mdCheckbox/index.css'
 import 'vue-material/dist/components/mdChips/index.css'
 import 'vue-material/dist/components/mdInputContainer/index.css'
 import 'vue-material/dist/components/mdLayout/index.css'
+import 'vue-material/dist/components/mdList/index.css'
+import 'vue-material/dist/components/mdMenu/index.css'
 import 'vue-material/dist/components/mdProgress/index.css'
+import 'vue-material/dist/components/mdSelect/index.css'
 import 'vue-material/dist/components/mdTable/index.css'
 import 'vue-material/dist/components/mdToolbar/index.css'
 
 Vue.use(VueMaterial.MdCore)
+Vue.use(VueMaterial.MdBackdrop)
 Vue.use(VueMaterial.MdButton)
+Vue.use(VueMaterial.MdCheckbox)
 Vue.use(VueMaterial.MdChips)
 Vue.use(VueMaterial.MdIcon)
 Vue.use(VueMaterial.MdInputContainer)
 Vue.use(VueMaterial.MdLayout)
+Vue.use(VueMaterial.MdList)
+Vue.use(VueMaterial.MdMenu)
 Vue.use(VueMaterial.MdProgress)
+Vue.use(VueMaterial.MdSelect)
 Vue.use(VueMaterial.MdTable)
 Vue.use(VueMaterial.MdToolbar)
 
--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Browse</h1>
+  <div>
+    <h1>Browse</h1>
+    <md-input-container>
+      <label for="indices">Indices</label>
+      <md-select multiple name="indices" id="indices" v-model="indices">
+        <md-option :value="index.index" :key="index.index" v-for="index in this.$store.state.indices">
+          {{ index.index }}
+        </md-option>
+      </md-select>
+    </md-input-container>
+  </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        indices: []
+      }
+    }
+  }
+</script>
