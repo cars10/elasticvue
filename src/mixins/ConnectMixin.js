857c4fdb98f6453d98422c18e31cff3ce03c5fc0
Carsten König <carstenkoenig92@gmail.com>
2017-12-07 18:02:58 +0100

    - only set client if needed

--- src/mixins/ConnectMixin.js
+++ src/mixins/ConnectMixin.js
@@ -1,21 +1,25 @@
 import ConnectService from '../services/elasticsearch/ConnectService'
 import ElasticsearchAdapter from '../services/ElasticsearchAdapter'
+import { isEmpty } from '../helpers'
 
 const ConnectMixin = {
   methods: {
     connectWithClient () {
       let connectService = new ConnectService(this.$store.state.connection.elasticsearchHost)
-      connectService.connect().then(
-        client => {
-          this.$store.commit('setElasticsearchClient', client)
-          let adapter = new ElasticsearchAdapter(client)
-          adapter.getCatIndices().then(
-            body => this.$store.commit('setIndices', body),
-            error => this.$store.commit('setErrorState', error)
-          )
-        },
-        error => this.$store.commit('setErrorState', error)
-      )
+
+      if (isEmpty(this.$store.state.connection.elasticsearchClient) === true) {
+        connectService.connect().then(
+          client => {
+            this.$store.commit('setElasticsearchClient', client)
+            let adapter = new ElasticsearchAdapter(client)
+            adapter.getCatIndices().then(
+              body => this.$store.commit('setIndices', body),
+              error => this.$store.commit('setErrorState', error)
+            )
+          },
+          error => this.$store.commit('setErrorState', error)
+        )
+      }
     }
   }
 }
