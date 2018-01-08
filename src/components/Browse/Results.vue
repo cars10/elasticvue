268173690151ce6b6384a2c04b1b55b88af03c13
Carsten König <carstenkoenig92@gmail.com>
2018-01-08 21:03:21 +0100

    - working on vuetify integration - browse/results - not working yet

--- src/components/Browse/Results.vue
+++ src/components/Browse/Results.vue
@@ -1,34 +1,21 @@
 <template>
   <div>
-    <md-table-card>
-      <md-table v-if="hits && hits.length > 0"
-                class="white-space--nowrap md-table--condensed md-table--striped md-table--clickable">
-        <md-table-header>
-          <md-table-row>
-            <md-table-head md-sort-by="_index">_index</md-table-head>
-            <md-table-head md-sort-by="_id">_id</md-table-head>
-            <md-table-head md-sort-by="_type">_type</md-table-head>
-            <md-table-head v-for="key in Object.keys(hits[0]._source)" :key="key">{{key}}</md-table-head>
-          </md-table-row>
-        </md-table-header>
-
-        <md-table-body>
-          <md-table-row v-for="hit in hits" :key="hit._type + '_' + hit._id" @click.native="onClick(hit)">
-            <md-table-cell>{{hit._index}}</md-table-cell>
-            <md-table-cell>{{hit._id}}</md-table-cell>
-            <md-table-cell>{{hit._type}}</md-table-cell>
-            <md-table-cell v-for="key in Object.keys(hit._source)" :key="hit._index + '_' + key">{{hit._source[key]}}
-            </md-table-cell>
-          </md-table-row>
-        </md-table-body>
-      </md-table>
-    </md-table-card>
+    <v-card>
+      <v-data-table v-if="hits && hits.length > 0"
+                    :rows-per-page-items="[10, 25, 100]"
+                    v-bind:headers="Object.keys(hits[0])"
+                    v-bind:items="hits">
+      </v-data-table>
+    </v-card>
   </div>
 </template>
 
 <script>
   export default {
     props: ['hits'],
+    data () {
+      return {}
+    },
     methods: {
       onClick (el) {
         this.$router.push({name: 'Document', params: {index: el._index, type: el._type, id: el._id}})
