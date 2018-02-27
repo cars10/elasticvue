9f86e02b669dfbddc42e87fa365181703de05141
Carsten König <carstenkoenig92@gmail.com>
2018-02-27 18:52:24 +0100

    adds default columns

--- src/components/Browse/Results.vue
+++ src/components/Browse/Results.vue
@@ -6,7 +6,10 @@
                     v-bind:headers="headers"
                     v-bind:items="hits">
         <template slot="items" slot-scope="item">
-          <td v-for="key in Object.keys(item.item._source)" :key="item.item._index + '_' + key">{{item.item._source[key]}}</td>
+          <td>{{ item.item._index }}</td>
+          <td>{{ item.item._id}}</td>
+          <td>{{ item.item._type}}</td>
+          <td v-for="key in keys" :key="item.item._index + '_' + key">{{item.item._source[key]}}</td>
         </template>
       </v-data-table>
     </v-card>
@@ -16,16 +19,24 @@
 <script>
   import { objectArrayUniqueKeys } from '../../helpers'
 
+  const DEFAULT_KEYS = ['_index', '_id', '_type']
+
   export default {
     props: ['hits'],
     data () {
       return {
-        headers: objectArrayUniqueKeys(this.hits, '_source').map(value => ({text: value, value: value}))
+        keys: objectArrayUniqueKeys(this.hits, '_source')
       }
     },
     methods: {
       onClick (el) {
         this.$router.push({name: 'Document', params: {index: el._index, type: el._type, id: el._id}})
+      }
+    },
+    computed: {
+      headers () {
+        let defaultKeyHeaders = DEFAULT_KEYS.map(value => ({text: value, value: value}))
+        return defaultKeyHeaders.concat(this.keys.map(value => ({text: value, value: value})))
       }
     }
   }
