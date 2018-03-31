c7db890b7a0b58f5c1c49632dc1ccfc5d2370c1b
Carsten König <carstenkoenig92@gmail.com>
2018-03-31 14:20:14 +0200

    change reload behaviour of index page when no connection

--- src/components/Indices.vue
+++ src/components/Indices.vue
@@ -1,45 +1,15 @@
 <template>
   <content-or-connect>
-    <v-card>
-      <v-card-title>
-        <h2>Indices</h2>
-        <v-btn flat icon v-on:click="loadIndices">
-          <v-icon>cached</v-icon>
-        </v-btn>
-      </v-card-title>
-      <v-divider></v-divider>
-      <indices-table :indices="indices" :loading="loading"></indices-table>
-    </v-card>
+    <indices-card></indices-card>
   </content-or-connect>
 </template>
 
 <script>
-  import IndicesTable from '@/components/Indices/IndicesTable'
+  import IndicesCard from '@/components/Indices/IndicesCard'
 
   export default {
-    data () {
-      return {
-        indices: [],
-        loading: false
-      }
-    },
     components: {
-      IndicesTable
-    },
-    created () {
-      this.loadIndices()
-    },
-    methods: {
-      loadIndices () {
-        this.loading = true
-
-        this.getElasticsearchAdapter().then(adapter => adapter.getCatIndices()).then(
-          indices => {
-            this.indices = indices
-            this.loading = false
-          }
-        ).catch(error => this.$store.commit('setErrorState', error))
-      }
+      IndicesCard
     }
   }
 </script>
