9e3722dea2c7bae8e81c367ba09af1a137c18a5b
Carsten König <carstenkoenig92@gmail.com>
2018-04-17 19:12:02 +0200

    check if production assets exist before serving them

--- prod-server.js
+++ prod-server.js
@@ -1,22 +1,28 @@
-/*
-  Incredibly simple Node.js and Express application server for serving static assets.
-  Given as an example from the React Router documentation (along with examples
-  using nginx and Apache):
-  - https://github.com/ReactTraining/react-router/blob/master/docs/guides/Histories.md#browserhistory
-*/
+const fs = require('fs')
+
+// Check if assets where built
+try {
+  fs.statSync('./dist/index.html')
+} catch (e) {
+  console.error('> Error: dist/index.html does not exist.')
+  console.error('> Please create the production assets by running:')
+  console.error('yarn build')
+  process.exit(1)
+}
 
 const express = require('express')
+const app = express()
 const path = require('path')
 const port = process.env.PORT || 8090
-const app = express()
 
-// serve static assets normally
+// serve static assets from /dist
 app.use(express.static(path.join(__dirname, '/dist')))
 
-// route every route to index.html
-app.get('*', function (request, response) {
+// respond to every other request with index.html
+app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
 app.listen(port)
-console.log('server started on localhost:' + port)
+
+console.log('server running on localhost:' + port)
