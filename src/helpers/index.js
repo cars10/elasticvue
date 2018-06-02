c20274a973525faa7d3017493764069cef99db29
Carsten König <carstenkoenig92@gmail.com>
2018-06-02 12:10:45 +0200

    removes unused function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,22 +1,5 @@
-import { ELASTICSEARCH_API_VERSIONS, DEFAULT_SEARCH_PARAMS } from '../consts'
+import { DEFAULT_SEARCH_PARAMS } from '../consts'
 import { isEmpty } from './utilities'
-
-/**
- * Return the correct api version for the elasticsearch version.
- * For example elasticsearch version '6.1' uses api version '6.x'
- * @param version
- * @returns {string}
- */
-export function mapElasticsearchApiVersion (version) {
-  switch (version) {
-    case ELASTICSEARCH_API_VERSIONS:
-      return version
-    case '6.1':
-      return '6.x'
-    default:
-      return '6.x'
-  }
-}
 
 export function normalizeSearchParams (params) {
   params.q = isEmpty(params.q) ? DEFAULT_SEARCH_PARAMS.q : params.q
