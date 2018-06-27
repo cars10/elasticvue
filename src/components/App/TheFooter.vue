bf1e8a6a49216a9ad194c6b13b17e0c03a605f7c
Carsten König <carstenkoenig92@gmail.com>
2018-06-27 19:35:20 +0200

    change footer structure

--- src/components/App/TheFooter.vue
+++ src/components/App/TheFooter.vue
@@ -1,21 +1,23 @@
 <template>
   <v-footer height="auto">
-    <v-layout row wrap py-3 px-3>
-      <v-flex xs4 align-center d-inline-flex>
-        <v-switch id="theme_select" label="Dark theme" v-model="theme"></v-switch>
+    <v-layout row wrap pa-4>
+      <v-flex xs4>
+        <div class="subheading">Settings</div>
+        <v-switch id="theme_select" class="my-2" label="Dark theme" v-model="theme" hide-details></v-switch>
+        <a @click="reset" title="Reset saved settings">Disconnect and reset</a>
       </v-flex>
 
       <v-flex xs4 text-xs-center>
-        {{version}} - &copy; {{ new Date().getFullYear()}}<br/>
-        <v-btn @click="reset" title="Reset saved settings">Reset</v-btn>
+        <div class="subheading">Elasticvue {{version}}</div>
+        &copy;{{ new Date().getFullYear()}} - Carsten K&ouml;nig<br/>
+        <a class="mt-2 d-inline-block" href="mailto:carstenkoenig92@gmail.com">carstenkoenig92@gmail.com</a><br/>
       </v-flex>
 
-      <v-flex xs4 text-xs-right align-center d-inline-flex>
-        <v-flex>
-          <a href="https://github.com/cars10/elasticvue" target="_blank" rel="nofollow">
-            <img src="../../../static/GitHub_Logo_White.png" alt="GithubLogo" v-if="this.$store.state.theme.dark">
-            <img src="../../../static/GitHub_Logo.png" alt="GithubLogo" v-else>
-          </a>
+      <v-flex xs4>
+        <v-flex text-xs-right>
+          <div class="subheading mb-2">Links</div>
+          <a href="https://elasticvue.com" target="_blank" rel="nofollow">Homepage</a><br/>
+          <a href="https://github.com/cars10/elasticvue" target="_blank" rel="nofollow">Github</a>
         </v-flex>
       </v-flex>
     </v-layout>
@@ -28,7 +30,7 @@
     computed: {
       version () {
         /* eslint-disable no-undef */
-        return `v${VERSION}`
+        return VERSION
       },
       theme: {
         get () {
