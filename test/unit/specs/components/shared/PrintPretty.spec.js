5184fc8fd2fb21abc457ea2a827d716caec72d43
Carsten König <carstenkoenig92@gmail.com>
2018-07-10 08:32:37 +0200

    fix spec

--- test/unit/specs/components/shared/PrintPretty.spec.js
+++ test/unit/specs/components/shared/PrintPretty.spec.js
@@ -1,3 +1,4 @@
+import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import VBtn from 'vuetify/es5/components/VBtn'
 import VIcon from 'vuetify/es5/components/VIcon'
@@ -6,21 +7,33 @@ import CodeEditor from '@/components/shared/CodeEditor'
 import ResizableContainer from '@/components/shared/ResizableContainer'
 import PrintPretty from '@/components/shared/PrintPretty'
 import { createLocalVue, mount } from '@vue/test-utils'
+import { theme } from '@/store/modules/theme'
 
 describe('PrintPretty.vue', () => {
-  let localVue = createLocalVue()
-  localVue.use(Vuetify, {
-    components: {
-      CodeEditor,
-      ResizableContainer,
-      VBtn,
-      VIcon,
-      VGrid
-    }
+  let localVue
+  let store
+
+  before(() => {
+    localVue = createLocalVue()
+
+    localVue = createLocalVue()
+    localVue.use(Vuex)
+    localVue.use(Vuetify, {
+      components: {
+        CodeEditor,
+        ResizableContainer,
+        VBtn,
+        VIcon,
+        VGrid
+      }
+    })
+    store = new Vuex.Store({
+      modules: {theme}
+    })
   })
 
   it('should render correct default contents', () => {
-    const wrapper = mount(PrintPretty, {localVue})
+    const wrapper = mount(PrintPretty, {localVue, store})
     const height = 'height: 600px'
     expect(wrapper.find('div > div').attributes().style).contains(height)
   })
