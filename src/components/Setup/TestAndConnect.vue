012d78ebc1f4aaefad92915f0b0cf135797334fa
Carsten König <carstenkoenig92@gmail.com>
2018-07-22 17:54:55 +0200

    increase input size for testAndConnect component

--- src/components/Setup/TestAndConnect.vue
+++ src/components/Setup/TestAndConnect.vue
@@ -3,33 +3,37 @@
     <h2 class="title mb-1">2. Test and connect</h2>
     <v-flex px-3>
       <v-form @submit.prevent="testConnection">
-        <v-flex d-inline-flex>
-          <v-text-field id="host"
-                        v-model="elasticsearchHost"
-                        :rules="[hostValid]"
-                        title="Host"
-                        label="Host"
-                        type="text"
-                        autofocus
-                        append-icon="clear"
-                        @keyup.ctrl.enter="connectIfTestSuccessfull"
-                        @click:append="resetElasticsearchHost"/>
-        </v-flex>
-        <v-btn id="test_connection"
-               :color="testConnectionColor"
-               :loading="testLoading"
-               :disabled="hostValid !== true"
-               type="submit">
-          Test connection
-        </v-btn>
+        <v-layout row>
+          <v-flex lg6>
+            <v-text-field id="host"
+                          v-model="elasticsearchHost"
+                          :rules="[hostValid]"
+                          title="Host"
+                          label="Host"
+                          type="text"
+                          autofocus
+                          append-icon="clear"
+                          @keyup.ctrl.enter="connectIfTestSuccessfull"
+                          @click:append="resetElasticsearchHost"/>
+          </v-flex>
+          <v-flex lg6>
+            <v-btn id="test_connection"
+                   :color="testConnectionColor"
+                   :loading="testLoading"
+                   :disabled="hostValid !== true"
+                   type="submit">
+              Test connection
+            </v-btn>
 
-        <v-btn id="connect"
-               :disabled="!testSuccess"
-               :color="connectColor"
-               :loading="connectLoading"
-               type="button"
-               @click.native="connectIfTestSuccessfull">Connect
-        </v-btn>
+            <v-btn id="connect"
+                   :disabled="!testSuccess"
+                   :color="connectColor"
+                   :loading="connectLoading"
+                   type="button"
+                   @click.native="connectIfTestSuccessfull">Connect
+            </v-btn>
+          </v-flex>
+        </v-layout>
       </v-form>
       <span class="grey--text">Hint: use <i>enter</i> to test connection, <i>ctrl+enter</i> to connect</span>
     </v-flex>
