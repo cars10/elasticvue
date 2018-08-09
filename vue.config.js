6082e19efc90d9c523c6ea9d3b040a739cdceca9
Carsten König <carstenkoenig92@gmail.com>
2018-08-09 17:46:35 +0200

    set permitted asset sizes

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ module.exports = {
   assetsDir: 'assets',
   productionSourceMap: false,
   lintOnSave: false,
+  configureWebpack: {
+    performance: {
+      maxEntrypointSize: 768000,
+      maxAssetSize: 768000
+    }
+  },
   chainWebpack: config => {
     config.plugin('define').tap(args => {
       args[0]['VERSION'] = JSON.stringify(process.env.npm_package_version)
