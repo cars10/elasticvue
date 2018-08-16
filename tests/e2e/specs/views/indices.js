2fe48a3ef7626552b5cd207d02eebba5aec0a58e
Carsten König <carstenkoenig92@gmail.com>
2018-08-16 11:30:35 +0200

    adds more index specs

--- tests/e2e/specs/views/indices.js
+++ tests/e2e/specs/views/indices.js
@@ -13,12 +13,40 @@ describe('Indices Page', () => {
       })
     })
 
+    it('can show indices', () => {
+      const indexName = 'name-1'
+      cy.createIndex(indexName)
+      cy.get('table').contains(indexName).closest('tr').get('button[title="Show"]').click()
+      cy.location().should(location => {
+        expect(location.pathname).to.match(/\/indices\/[a-z0-9]*/)
+      })
+      cy.contains(indexName)
+    })
+
     it('can delete indices', () => {
       const indexName = 'name-1'
       cy.createIndex(indexName)
       cy.get('table').contains(indexName).closest('tr').get('button[title="Delete"]').click()
       cy.get('table').should(table => {
         expect(table).not.to.contain(indexName)
+      })
+    })
+  })
+
+  describe('table', () => {
+    it('can reload', () => {
+      cy.visit('/indices')
+
+      cy.get('table').should(table => {
+        expect(table).to.contain('No data available')
+      })
+
+      const indexName = 'name-1'
+      cy.request('PUT', 'http://localhost:' + Cypress.env('ES_PORT').toString() + '/' + indexName)
+      cy.get('#reload-indices').click()
+
+      cy.get('table').should(table => {
+        expect(table).to.contain(indexName)
       })
     })
   })
