1d5d4a673d33ccc8231704c1fdb4a2ddfef9eea1
Carsten König <carstenkoenig92@gmail.com>
2018-09-18 18:09:17 +0200

    adds icon

---
+++ build_chrome_extension.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+yarn build
+
+# make chrome extension
+rm -rf chrome_extension/index.html chrome_extension/assets
+cp -r dist/* chrome_extension/
+mkdir -p chrome_extension/assets/img/logo
+cp -r public/images/logo/*_blue.png chrome_extension/assets/img/logo
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+
 module.exports = {
   assetsDir: 'assets',
   productionSourceMap: false,
@@ -6,7 +8,24 @@ module.exports = {
     performance: {
       maxEntrypointSize: 768000,
       maxAssetSize: 768000
-    }
+    },
+    plugins: [
+      new FaviconsWebpackPlugin({
+        logo: './public/images/logo/logo_128_blue.png',
+        icons: {
+          android: false,
+          appleIcon: false,
+          appleStartup: false,
+          coast: false,
+          favicons: true,
+          firefox: false,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      })
+    ]
   },
   chainWebpack: config => {
     config.plugin('define').tap(args => {
