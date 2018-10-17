edf8e3aa64f441425b8e42714bc52fbc22f4dabb
Carsten König <carstenkoenig92@gmail.com>
2018-10-17 20:41:35 +0200

    adds auto reloading for reload buttons

--- src/components/shared/ReloadButton.vue
+++ src/components/shared/ReloadButton.vue
@@ -1,7 +1,15 @@
 <template>
-  <v-btn :title="title" :id="id" flat icon @click="action">
-    <v-icon>cached</v-icon>
-  </v-btn>
+  <div class="d-inline-block reload-button">
+    <v-btn :title="title" :id="id" flat icon @click="action">
+      <v-icon>cached</v-icon>
+    </v-btn>
+    <v-select :items="timerSettings"
+              v-model="timerSetting"
+              item-value="value"
+              item-text="text"
+              hide-details
+              class="mt-0 pt-0 d-inline-block"/>
+  </div>
 </template>
 
 <script>
@@ -20,6 +28,44 @@
       title: {
         type: String,
         default: 'Reload'
+      },
+      timerSettings: {
+        type: Array,
+        default: () => {
+          return [
+            { text: 'None', value: null },
+            { text: '5s', value: 5 },
+            { text: '15s', value: 15 },
+            { text: '30s', value: 30 },
+            { text: '60s', value: 60 }
+          ]
+        }
+      }
+    },
+    data () {
+      return {
+        timerSetting: null,
+        timer: null
+      }
+    },
+    watch: {
+      timerSetting (value) {
+        clearTimeout(this.timer)
+        if (value) this.setTimer()
+      }
+    },
+    created () {
+      // this.setTimer()
+    },
+    destroyed () {
+      clearTimeout(this.timer)
+    },
+    methods: {
+      setTimer () {
+        this.timer = setTimeout(() => {
+          this.action()
+          this.setTimer()
+        }, this.timerSetting * 1000)
       }
     }
   }
