d2d9126608844b000d4b0d459aca76808db9e401
Carsten König <carstenkoenig92@gmail.com>
2018-10-31 17:27:13 +0100

    add wrap lines to all printpretty components

--- src/components/shared/PrintPretty.vue
+++ src/components/shared/PrintPretty.vue
@@ -1,18 +1,22 @@
 <template>
-  <v-flex v-if="resizable" py-2>
-    <resizable-container :initial-height="initialHeight">
-      <code-editor :value="document" read-only/>
-    </resizable-container>
-  </v-flex>
-  <div v-else :style="style">
-    <v-flex right>
-      <v-btn-toggle v-model="wrapLines" class="ma-2">
-        <v-btn :value="true" title="Wrap lines">
-          <v-icon>wrap_text</v-icon>
-        </v-btn>
-      </v-btn-toggle>
+  <div>
+    <div class="clearfix">
+      <v-flex right>
+        <v-btn-toggle v-model="wrapLines" class="ma-2">
+          <v-btn :value="true" title="Wrap lines">
+            <v-icon>wrap_text</v-icon>
+          </v-btn>
+        </v-btn-toggle>
+      </v-flex>
+    </div>
+    <v-flex v-if="resizable" py-2>
+      <resizable-container :initial-height="initialHeight">
+        <code-editor :value="document" read-only/>
+      </resizable-container>
     </v-flex>
-    <code-editor :value="document" :wrap-lines="wrapLines" read-only/>
+    <div v-else :style="style">
+      <code-editor :value="document" :wrap-lines="wrapLines" read-only/>
+    </div>
   </div>
 </template>
 
