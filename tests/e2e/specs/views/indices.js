0060a5bcd1eb7b3892ab93ad37a6a03e6eecfdb9
Carsten König <carstenkoenig92@gmail.com>
2018-11-02 19:10:02 +0100

    fix index specs

--- tests/e2e/specs/views/indices.js
+++ tests/e2e/specs/views/indices.js
@@ -17,7 +17,8 @@ describe('Indices page', () => {
       const indexName = 'name-1'
       cy.createIndex(indexName)
       cy.flushIndices().then(() => {
-        cy.get('table').contains(indexName).closest('tr').get('a[title="Show index"]').click()
+        cy.get('table').contains(indexName).closest('tr').get('button[title="Options"]').click()
+        cy.get('table').contains(indexName).closest('tr').get('a').contains('info').click()
         cy.contains('mappings').should('exist')
       })
     })
@@ -25,7 +26,8 @@ describe('Indices page', () => {
     it('can delete indices', () => {
       const indexName = 'name-1'
       cy.createIndex(indexName)
-      cy.get('table').contains(indexName).closest('tr').get('button[title="Delete"]').click()
+      cy.get('table').contains(indexName).closest('tr').get('button[title="Options"]').click()
+      cy.get('table').contains(indexName).closest('tr').get('a').contains('Delete').click()
       cy.get('table').should(table => {
         expect(table).not.to.contain(indexName)
       })
