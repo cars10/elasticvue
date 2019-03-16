9a683bd864ed274bd7bfe5e8c558e3e9f675b187
Carsten König <carstenkoenig92@gmail.com>
2019-03-16 12:08:21 +0100

    fix api browser select default value

--- src/store/modules/queryApiBrowser.js
+++ src/store/modules/queryApiBrowser.js
@@ -1,9 +1,7 @@
-import { HTTP_METHODS } from '../../consts'
-
 export const queryApiBrowser = {
   namespaced: true,
   state: {
-    method: HTTP_METHODS[0],
+    method: 'info',
     stringifiedParams: JSON.stringify({ format: 'json' }, null, '\t')
   },
   mutations: {
