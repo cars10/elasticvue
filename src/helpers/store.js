203cf4d7c524d3deeeb4453a53e4171299d16f97
Carsten König <carstenkoenig92@gmail.com>
2019-03-16 12:26:54 +0100

    remove unused functionality for mapVuexAccessor helper

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -2,21 +2,16 @@ import { capitalize } from './'
 
 /**
  * Generates get() and set() functions for vuex states. It does *not* check if your states and mutations actually exist.
- * Accepts an optional namespace and an object containing the get/set pairs like this:
- * {yourKey: setYourKey}, where `yourKey` is the states name and `setYourKey` is the name of the corresponding mutation.
- *
- * Alternatively pass an array of strings for states, this will generate {yourstring: setYourstring}
+ * Pass an array of strings for states, this will generate {yourstring: setYourstring}
  *
  * @example
- *  mapVuexAccessors('search', {filter: 'setFilter'})
- *  // is the same as:
  *  mapVuexAccessors('search', ['filter'])
  * @usage
  *  use in combination with the spread operator ... in your computed properties:
  *  ...,
  *  computed () {
  *    something () {//..},
- *    ...mapVuexAccessors('search', {filter: 'setFilter'})
+ *    ...mapVuexAccessors('search', ['filter'])
  *  },
  *  ...
  * @param namespace (optional)
@@ -31,41 +26,26 @@ export const mapVuexAccessors = function (namespace, states) {
 
   let methods = {}
 
-  if (Array.isArray(states)) {
-    states.forEach(function (key) {
-      methods[key] = {
-        get () {
+  states.forEach(function (key) {
+    methods[key] = {
+      get () {
+        /* istanbul ignore next */
+        return namespace ? this.$store.state[namespace][key] : this.$store.state[key]
+      },
+      set (value) {
+        /* istanbul ignore next */
+        let capitalizedKey = capitalize(key)
+        /* istanbul ignore next */
+        if (namespace) {
           /* istanbul ignore next */
-          return namespace ? this.$store.state[namespace][key] : this.$store.state[key]
-        },
-        set (value) {
+          this.$store.commit(`${namespace}/set${capitalizedKey}`, value)
+        } else {
           /* istanbul ignore next */
-          let capitalizedKey = capitalize(key)
-          /* istanbul ignore next */
-          if (namespace) {
-            /* istanbul ignore next */
-            this.$store.commit(`${namespace}/set${capitalizedKey}`, value)
-          } else {
-            /* istanbul ignore next */
-            this.$store.commit(`set${capitalizedKey}`, value)
-          }
+          this.$store.commit(`set${capitalizedKey}`, value)
         }
       }
-    })
-  } else {
-    Object.keys(states).forEach(function (key) {
-      methods[key] = {
-        get () {
-          /* istanbul ignore next */
-          return namespace ? this.$store.state[namespace][key] : this.$store.state[key]
-        },
-        set (value) {
-          /* istanbul ignore next */
-          namespace ? this.$store.commit(`${namespace}/${states[key]}`, value) : this.$store.commit(states[key], value)
-        }
-      }
-    })
-  }
+    }
+  })
 
   return methods
 }
--- tests/unit/helpers/store.spec.js
+++ tests/unit/helpers/store.spec.js
@@ -6,37 +6,16 @@ describe('helpers/mapVuexAccessors', () => {
       expect(mapVuexAccessors()).toEqual({})
     })
 
-    it('returns empty object when empty object is passed', () => {
-      expect(mapVuexAccessors({})).toEqual({})
-    })
-
     it('returns empty object when empty array is passed', () => {
       expect(mapVuexAccessors([])).toEqual({})
     })
 
     it('returns empty object when empty namespace object is passed', () => {
-      expect(mapVuexAccessors('', {})).toEqual({})
+      expect(mapVuexAccessors('', [])).toEqual({})
     })
   })
 
   describe('creates correct output without namespace', () => {
-    it('returns a correct object when called with states object', () => {
-      const states = { search: 'setSearch' }
-      const expected = {
-        search: {
-          get () {
-            return this.$store.state['search']
-          },
-          set (value) {
-            this.$store.commit('setSearch', value)
-          }
-        }
-      }
-      const actual = mapVuexAccessors(states)
-      expect(Object.keys(actual)).toEqual(Object.keys(expected))
-      expect(Object.keys(actual.search)).toEqual(Object.keys(expected.search))
-    })
-
     it('returns a correct object when called with states array', () => {
       const states = ['search']
       const expected = {
@@ -56,26 +35,6 @@ describe('helpers/mapVuexAccessors', () => {
   })
 
   describe('creates correct output with namespace', () => {
-    it('returns a correct object when called with states object', () => {
-      const namespace = 'search'
-      const state = 'filter'
-      const stateUpcase = 'Filter'
-      const states = { [state]: `set${stateUpcase}` }
-      const expected = {
-        [state]: {
-          get () {
-            return this.$store.state[namespace][state]
-          },
-          set (value) {
-            this.$store.commit(`${namespace}/set${stateUpcase}`, value)
-          }
-        }
-      }
-      const actual = mapVuexAccessors(namespace, states)
-      expect(Object.keys(actual)).toEqual(Object.keys(expected))
-      expect(Object.keys(actual.filter)).toEqual(Object.keys(expected.filter))
-    })
-
     it('returns a correct object when called with states array', () => {
       const namespace = 'search'
       const state = 'filter'
