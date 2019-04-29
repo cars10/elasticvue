c5786983d21f5c25f59cdc68d4203ba95b4ba24a
Carsten König <carstenkoenig92@gmail.com>
2019-04-29 15:03:38 +0200

    adds snapshots for btnGroup spec

--- tests/unit/components/shared/BtnGroup.spec.js
+++ tests/unit/components/shared/BtnGroup.spec.js
@@ -4,8 +4,7 @@ import { mount } from '@vue/test-utils'
 describe('components/shared/BtnGroup.vue', () => {
   it('should render correct default contents', () => {
     const wrapper = mount(BtnGroup)
-    expect(wrapper.classes()).toContainEqual('elevation-1')
-    expect(wrapper.classes()).toContainEqual('btn-group')
+    expect(wrapper.element).toMatchSnapshot()
   })
 
   it('should add the small group', () => {
@@ -14,6 +13,6 @@ describe('components/shared/BtnGroup.vue', () => {
         small: true
       }
     })
-    expect(wrapper.classes()).toContain('btn-group--small')
+    expect(wrapper.element).toMatchSnapshot()
   })
 })
