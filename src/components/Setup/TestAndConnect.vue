c51ea76089b49cbfd3e5a566471640728573660f
Carsten König <carstenkoenig92@gmail.com>
2019-07-11 15:01:38 +0200

    adjust button margins for setup form

--- src/components/Setup/TestAndConnect.vue
+++ src/components/Setup/TestAndConnect.vue
@@ -21,6 +21,7 @@
                    :color="testConnectionColor"
                    :loading="testLoading"
                    :disabled="hostValid !== true"
+                   class="mr-2 mt-2"
                    type="submit">
               Test connection
             </v-btn>
@@ -29,6 +30,7 @@
                    :disabled="hostValid !== true"
                    :color="connectColor"
                    :loading="connectLoading"
+                   class="mt-2"
                    type="button"
                    @click.native="connect">Connect
             </v-btn>
