1f9faf2114951460c0e66ec8792e64d50aa0c991
Carsten König <carstenkoenig92@gmail.com>
2019-08-05 11:29:27 +0200

    fix some more cols

--- src/components/shared/PrintPretty.vue
+++ src/components/shared/PrintPretty.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
     <v-row>
-      <v-col class="my-0" cols="12" md="9">
-        <h2 v-if="caption" class="title pt-4">{{caption}}</h2>
+      <v-col class="my-0 py-0" cols="12" md="9">
+        <h2 v-if="caption" class="title">{{caption}}</h2>
       </v-col>
-      <v-col class="my-0 mx-2" cols="12" md="3">
-        <v-btn-toggle v-model="wrapLines" class="float-right">
+      <v-col class="my-0 py-0" cols="12" md="3">
+        <v-btn-toggle v-model="wrapLines" class="float-right v-btn-toggle--small">
           <v-btn :value="true" title="Wrap lines">
             <v-icon>mdi-wrap</v-icon>
           </v-btn>
         </v-btn-toggle>
       </v-col>
     </v-row>
-    <v-col v-if="resizable" class="py-2">
+    <div v-if="resizable" class="py-1">
       <resizable-container :initial-height="initialHeight">
         <code-editor :value="document" read-only/>
       </resizable-container>
-    </v-col>
+    </div>
     <div v-else :style="style" class="pt-2">
       <code-editor :value="document" :wrap-lines="wrapLines" read-only/>
     </div>
