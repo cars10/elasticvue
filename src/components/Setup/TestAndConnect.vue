37cb05c51bc83f3c1afe8a646f3d0f158943b566
Carsten König <carstenkoenig92@gmail.com>
2019-11-29 09:13:38 +0100

    increase width of host in setup

--- src/components/Setup/TestAndConnect.vue
+++ src/components/Setup/TestAndConnect.vue
@@ -3,41 +3,38 @@
     <h2 class="title mb-1">2. Test and connect</h2>
     <div class="px-4">
       <v-form @submit.prevent="testConnection">
-        <v-row>
-          <v-col lg="6">
-            <v-text-field id="host"
-                          :rules="[hostValid]"
-                          v-model="elasticsearchHost"
-                          append-icon="mdi-close"
-                          autofocus
-                          label="Host"
-                          title="Host"
-                          type="text"
-                          @click:append="resetElasticsearchHost"
-                          @keyup.ctrl.enter="connect"/>
-          </v-col>
-          <v-col lg="6">
-            <v-btn id="test_connection"
-                   :color="testConnectionColor"
-                   :disabled="hostValid !== true"
-                   :loading="testLoading"
-                   class="mr-2 mt-2"
-                   type="submit">
-              Test connection
-            </v-btn>
+        <v-text-field id="host"
+                      :rules="[hostValid]"
+                      v-model="elasticsearchHost"
+                      append-icon="mdi-close"
+                      autofocus
+                      label="Host"
+                      title="Host"
+                      type="text"
+                      @click:append="resetElasticsearchHost"
+                      @keyup.ctrl.enter="connect"/>
+        <div class="mb-4">
+          <v-btn id="test_connection"
+                 :color="testConnectionColor"
+                 :disabled="hostValid !== true"
+                 :loading="testLoading"
+                 class="mr-2 mt-2"
+                 type="submit">
+            Test connection
+          </v-btn>
 
-            <v-btn id="connect"
-                   :color="connectColor"
-                   :disabled="hostValid !== true"
-                   :loading="connectLoading"
-                   class="mt-2"
-                   type="button"
-                   @click.native="connect">Connect
-            </v-btn>
-          </v-col>
-        </v-row>
+          <v-btn id="connect"
+                 :color="connectColor"
+                 :disabled="hostValid !== true"
+                 :loading="connectLoading"
+                 class="mt-2"
+                 type="button"
+                 @click.native="connect">Connect
+          </v-btn>
+        </div>
       </v-form>
-      <p class="grey--text">To connect with credentials use http://username:password@host syntax. Don't encode special characters in the password!</p>
+      <p class="grey--text">To connect with credentials use http://username:password@host syntax. Don't encode special
+        characters in the password!</p>
     </div>
 
     <div v-if="hasError" class="px-4">
