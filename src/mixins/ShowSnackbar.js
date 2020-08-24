ef0e2197205b3865628ddfdd89ba04cae922668a
Carsten König <carstenkoenig92@gmail.com>
2020-08-24 15:11:09 +0200

    change some snackbar default values

--- src/mixins/ShowSnackbar.js
+++ src/mixins/ShowSnackbar.js
@@ -1,9 +1,11 @@
 import store from '@/store'
 
 export const showSuccessSnackbar = function (props) {
-  store.commit('snackbar/show', Object.assign({}, { timeout: 5000, color: 'success' }, props))
+  store.commit('snackbar/show', Object.assign({}, { timeout: 5000, color: 'green' }, props))
 }
 
 export const showErrorSnackbar = function (props) {
-  store.commit('snackbar/show', Object.assign({}, { timeout: 10000, color: 'error' }, props))
+  if (props.text) console.error(props.text)
+  if (props.additionalText) console.error(props.additionalText)
+  store.commit('snackbar/show', Object.assign({}, { timeout: 20000, color: 'red' }, props))
 }
