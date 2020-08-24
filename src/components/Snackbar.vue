bb6da1438296e04827f12022854caf0ff5e6dc25
Carsten König <carstenkoenig92@gmail.com>
2020-08-24 15:11:20 +0200

    adds "copy error" button to error snackbars

--- src/components/Snackbar.vue
+++ src/components/Snackbar.vue
@@ -4,17 +4,21 @@
               :timeout="timeout"
               v-model="visible"
               right>
-    {{text}}
+    <strong>{{ text }}</strong>
     <template v-if="additionalText">
-      <br>
-      {{additionalText}}
+      <p class="mb-0">
+        {{ additionalText }}
+      </p>
+      <div v-if="color === 'red'" class="mt-2">
+        <v-btn color="" @click.native="copy">Copy error</v-btn>
+        <input ref="copyTextArea" :value="additionalText" type="text" style="width:1px">
+      </div>
     </template>
-    <v-btn text @click.native="visible = false">Close</v-btn>
   </v-snackbar>
 </template>
 
 <script>
-  import { mapVuexAccessors } from '../helpers/store'
+  import { mapVuexAccessors } from '@/helpers/store'
   import { mapState } from 'vuex'
 
   export default {
@@ -22,6 +26,15 @@
     computed: {
       ...mapVuexAccessors('snackbar', ['visible']),
       ...mapState('snackbar', ['text', 'additionalText', 'timeout', 'color'])
+    },
+    methods: {
+      copy () {
+        const textarea = this.$refs.copyTextArea
+        textarea.select()
+        textarea.setSelectionRange(0, 99999)
+        document.execCommand('copy')
+        this.visible = false
+      }
     }
   }
 </script>
