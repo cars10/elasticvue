22e70837c858e85c6398dbf631f1343516c7c0b3
Carsten König <carstenkoenig92@gmail.com>
2020-08-27 15:42:17 +0200

    revert wrong change

--- src/components/Indices/IndexRow.vue
+++ src/components/Indices/IndexRow.vue
@@ -179,7 +179,7 @@
       },
       async loadAliases () {
         this.aliasesLoading = true
-        let adapter = esAdapter()
+        let adapter = await esAdapter()
         adapter.indexGetAlias({ index: this.index.index }).then(r => {
           if (!r[this.index.index] || !r[this.index.index].aliases) {
             this.aliases = []
