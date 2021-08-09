719ab07fe523224184934601f783ef022b21b79e
Carsten König <carstenkoenig92@gmail.com>
2021-08-09 15:31:34 +0200

    set vuetify i18n

--- src/vuetify.js
+++ src/vuetify.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import en from 'vuetify/lib/locale/en'
+import zhHans from 'vuetify/lib/locale/zh-Hans'
+
 import store from '@/store'
 import '@mdi/font/css/materialdesignicons.css'
 import colors from 'vuetify/lib/util/colors'
@@ -12,6 +15,17 @@ const basicColors = {
   'primary-button': colors.blue.darken2
 }
 
+const getVuetifyLocale = () => {
+  const currentLocale = store.state.language.language
+
+  switch (currentLocale) {
+    case 'cn':
+      return 'zhHans'
+    default:
+      return currentLocale
+  }
+}
+
 const vuetify = new Vuetify({
   icons: { iconfont: 'mdi' },
   theme: {
@@ -20,6 +34,10 @@ const vuetify = new Vuetify({
       light: basicColors
     },
     dark: store.state.theme.dark
+  },
+  lang: {
+    locales: { en, zhHans },
+    current: getVuetifyLocale()
   }
 })
 
