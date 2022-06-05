372631d1802f3fb467b01104d6476f8cf03acd8d
Carsten König <carstenkoenig92@gmail.com>
2022-06-05 12:30:54 +0200

    update tauri icons

--- src/consts.js
+++ src/consts.js
@@ -17,7 +17,7 @@ export const LOCALSTORAGE_KEY = 'elasticvuex'
 export const INDEXEDDB_NAME = 'elasticvue'
 export const DEFAULT_SEARCH_QUERY = '{\n\t"query": {\n\t\t"query_string": {\n\t\t\t"query": "*"\n\t\t}\n\t},\n\t"size": 10,\n\t"from": 0,\n\t"sort": []\n}'
 export const DEFAULT_HIDE_INDICES_REGEX = '^\\..*'
-export const DESKTOP_BUILD = true // !!process.env.VUE_APP_DESKTOP_BUILD
+export const DESKTOP_BUILD = !!process.env.VUE_APP_DESKTOP_BUILD
 
 export const REQUEST_DEFAULT_HEADERS = {
   Accept: 'application/json',
