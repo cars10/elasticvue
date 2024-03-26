98d1030c31cd3b695364322854528bbbb0d1f41c
Carsten König <carstenkoenig92@gmail.com>
2024-03-26 20:01:35 +0100

    refactor

--- src/components/search/SearchDocuments.vue
+++ src/components/search/SearchDocuments.vue
@@ -18,48 +18,7 @@
                      :label="t('search.form.query.label')"
                      @keydown.esc="searchStore.q = '*'" />
 
-            <q-btn flat no-caps size="sm" class="q-px-xs q-mt-sm" @click="examplesOpen = !examplesOpen">
-              <q-icon :name="examplesOpen ? 'expand_less' : 'expand_more'" />
-              {{ t('search.form.search_examples.button') }}
-            </q-btn>
-
-            <q-slide-transition>
-              <div v-if="examplesOpen" class="q-pt-md">
-                <!-- eslint-disable-next-line vue/no-v-html -->
-                <span v-html="t('search.form.search_examples.hint')" />
-
-                <q-markup-table flat>
-                  <thead>
-                    <tr>
-                      <th>Query</th>
-                      <th>Description</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td class="font-mon">server error</td>
-                      <td>Get all documents that contain the words "server" or "error" in any field</td>
-                    </tr>
-                    <tr>
-                      <td class="font-mon">_id:1</td>
-                      <td>Get all documents where the <strong>_id</strong> field is <strong>1</strong></td>
-                    </tr>
-                    <tr>
-                      <td class="font-mon">full_name:"John Smith"</td>
-                      <td>Get all documents where the <strong>full_name</strong> field is exactly <strong>John Smith</strong>, including whitespace</td>
-                    </tr>
-                    <tr>
-                      <td class="font-mon">full_name:"John Smith"</td>
-                      <td>Get all documents where the <strong>first_name</strong> field is <strong>John</strong> or <strong>Pete</strong></td>
-                    </tr>
-                    <tr>
-                      <td class="font-mon">age:>25</td>
-                      <td>Get all documents where the <strong>age</strong> field is greater than <strong>25</strong></td>
-                    </tr>
-                  </tbody>
-                </q-markup-table>
-              </div>
-            </q-slide-transition>
+            <search-examples />
           </div>
 
           <div class="col">
@@ -113,17 +72,17 @@
 </template>
 
 <script setup lang="ts">
-  import { defineAsyncComponent, onMounted, ref } from 'vue'
+  import { defineAsyncComponent, onMounted } from 'vue'
   import IndexFilter from '../shared/IndexFilter.vue'
   import LoaderStatus from '../shared/LoaderStatus.vue'
   import ResizableContainer from '../shared/ResizableContainer.vue'
   import SearchResultsTable from './SearchResultsTable.vue'
   import { useSearchDocuments } from '../../composables/components/search/SearchDocuments'
   import { useTranslation } from '../../composables/i18n.ts'
+  import SearchExamples from './SearchExamples.vue'
 
   const CodeEditor = defineAsyncComponent(() => import('../shared/CodeEditor.vue'))
 
-  const examplesOpen = ref(false)
 
   const t = useTranslation()
   const {
