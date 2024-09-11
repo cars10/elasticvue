4f8cbf16573a476f3c1bf5b468e512c62d6ac2d6
Carsten König <carstenkoenig92@gmail.com>
2024-09-11 20:49:03 +0200

    cleanup i18n and add i18n lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,53 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
+import vueI18n from '@intlify/eslint-plugin-vue-i18n'
 
 export default [
-    ...compat.extends("plugin:vue/vue3-recommended", "@vue/eslint-config-typescript"),
-    {
-        rules: {
-            "vue/max-attributes-per-line": "off",
-            "vue/first-attribute-linebreak": "off",
-            "vue/html-closing-bracket-newline": "off",
-            "vue/singleline-html-element-content-newline": "off",
-            quotes: ["error", "single"],
-        },
-        files: ["src/**/*.js", "src/**/*.vue", "src/**/*.ts"]
+  ...compat.extends('plugin:vue/vue3-recommended', '@vue/eslint-config-typescript'),
+  ...vueI18n.configs['flat/recommended'],
+  {
+    rules: {
+      'vue/max-attributes-per-line': 'off',
+      'vue/first-attribute-linebreak': 'off',
+      'vue/html-closing-bracket-newline': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      '@intlify/vue-i18n/no-raw-text': 'off',
+      '@intlify/vue-i18n/no-v-html': 'off',
+      '@intlify/vue-i18n/no-html-messages': 'off',
+      '@intlify/vue-i18n/key-format-style': ['error', 'snake_case'],
+      '@intlify/vue-i18n/no-duplicate-keys-in-locale': ['error'],
+      '@intlify/vue-i18n/no-dynamic-keys': 'error',
+      '@intlify/vue-i18n/no-missing-keys-in-other-locales': 'error',
+      '@intlify/vue-i18n/no-unknown-locale': ['error', { locales: ['en', 'fr', 'cn'] }],
+      '@intlify/vue-i18n/no-unused-keys': ['error', {
+        'src': './src',
+        'extensions': ['.js', '.vue', '.ts'],
+        'enableFix': true
+      }],
+      quotes: ['error', 'single'],
     },
-];
+    files: ['src/**/*.{js,vue,ts,json}'],
+    settings: {
+      'vue-i18n': {
+        localeDir: {
+          pattern: './src/locales/*.json',
+          localeKey: 'file'
+        },
+        messageSyntaxVersion: '^9.0.0'
+      }
+    },
+    languageOptions: {
+      ecmaVersion: 2020
+    }
+  },
+]
