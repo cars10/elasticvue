47aa1d857fcc2bbb1eb8ec16e0b147bb8bd2c7da
Carsten König <carstenkoenig92@gmail.com>
2025-03-18 19:10:32 +0100

    fix input ui #2

--- src/components/shared/CustomInput.vue
+++ src/components/shared/CustomInput.vue
@@ -1,9 +1,15 @@
 <template>
-  <q-input v-bind="props" spellcheck="false" hide-bottom-space />
+  <q-input v-bind="props" spellcheck="false" hide-bottom-space>
+    <template v-if="slots.hint" #hint>
+      <slot name="hint"></slot>
+    </template>
+  </q-input>
 </template>
 
 <script setup lang="ts">
   import { QInputProps } from 'quasar'
+  import { useSlots } from 'vue'
 
+  const slots = useSlots()
   const props = defineProps<QInputProps>()
 </script>
