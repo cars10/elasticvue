a8e20b472d0798724a12547d545d42da5558c7ec
Carsten König <koenig.carsten@territory.group>
2025-05-05 09:47:28 +0200

    fix opening links in desktop app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,6 +21,7 @@ pub fn run() {
     // std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_clipboard_manager::init())
